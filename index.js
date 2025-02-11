490e0e1e29f91b8cd50db835b2470369c0f0babc
CrispysCode <pr0ject0dinx@gmail.com>
2025-02-11 08:58:19 -0600

    Finished the gameboard & player object using IIFE

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 //Gameboard factory
 
-const Gameboard = (function() {
-    const board = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
-    ];    
-})
+const Gameboard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];    
+
+    const getBoard = () => board.slice();
+
+    const setCell = (index, playerMark) => {
+        if (index >0 && index < 9 && board.index === "") {
+            return true;
+        }
+        return false;
+    }
+
+    const resetBoard = () => {
+        board.textContent = board;
+    }
+    return { getBoard, setCell, resetBoard }
+})();
 
 //Player Object
 
 const Player = (name, mark) => {
-    return {
-        name,
-        mark
-    };
+   const getName = () => name;
+   const getMark = () => mark;
+
+   return { getName, getMark };
 };
