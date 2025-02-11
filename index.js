7dbf74790cc095303a875675eecdbe81d72fd927
CrispysCode <pr0ject0dinx@gmail.com>
2025-02-11 08:51:35 -0600

    Created basic html and js syntax

---
+++ index.js
@@ -0,0 +1,18 @@
+//Gameboard factory
+
+const Gameboard = (function() {
+    const board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];    
+})
+
+//Player Object
+
+const Player = (name, mark) => {
+    return {
+        name,
+        mark
+    };
+};
